6463ccdee9f293ca3156fdc2e9f44a59469d2976
liuxian <liuxiangogo@gmail.com>
2022-01-20 22:14:08 +0800

    easy first infer 解法

--- type-challenges/14-easy-first/template.ts
+++ type-challenges/14-easy-first/template.ts
@@ -1,5 +1,6 @@
 // type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
-type First<T extends any[]> = T extends [] ? never : T[0];
+// type First<T extends any[]> = T extends [] ? never : T[0];
+type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never
 
 // js 解法
 function first(arrs) {
